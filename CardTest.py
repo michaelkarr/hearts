3b556ceaec679ba6d44e9ba984e604bd48b716ec
Michael Karr <mkarr1@yahoo.com>
2019-10-10 09:18:20 -0700

    print fix

--- CardTest.py
+++ CardTest.py
@@ -6,7 +6,7 @@ unit test like format. Shows correctness of the operators,
 which compare cards relative to each others' ranks.
 '''
 
-print 'Starting Card opertor tests...\n'
+print('Starting Card opertor tests...\n')
 
 clubs = 0
 diamonds = 1
@@ -43,12 +43,12 @@ c = Card(12, diamonds)
 c1 = Card(12, clubs)
 
 
-print '\nc:', c
-print 'c1:', c1
+print('\nc:', c)
+print('c1:', c1)
 
-print "c == c1", c == c1
-print "c >= c1", c >= c1
-print "c <= c1", c <= c1
-print "c < c1", c < c1
-print "c > c1", c > c1
+print("c == c1", c == c1)
+print("c >= c1", c >= c1)
+print("c <= c1", c <= c1)
+print("c < c1", c < c1)
+print("c > c1", c > c1)
 
